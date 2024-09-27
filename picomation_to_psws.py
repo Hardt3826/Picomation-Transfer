018725b12b72f4b030a9c6745fd7792d525c9c15
Evan Hardt <echardt@crimson.ua.edu>
2024-09-27 12:54:20 -0500

    Work Session 3

--- picomation_to_psws.py
+++ picomation_to_psws.py
@@ -8,17 +8,41 @@
 # ----------------------------------------------------------------------------
 
 import os
+import shutil
 
 from paramiko import AutoAddPolicy, SSHClient
+import zipfile
+from zipfile import ZipFile
+
 
 def fetch_magData(Station):
-    cwd= os.chdir("C:/Users/Evan Hardt/Desktop/Picomation Data")
+    os.chdir("C:/Users/Evan Hardt/Desktop/Picomation Data")
+    os.mkdir(Station['Name'])
+    cwd= os.chdir("C:/Users/Evan Hardt/Desktop/Picomation Data/" + Station['Name'])
+    Station['Local_Path']= os.getcwd()
     os.system("wsl -e wget -r -np -nH --cut-dirs=2 -R index.html --no-check- " + Station["Picomation Dir"])
     for file in os.listdir(cwd):
         if not file.endswith('.log'):
             os.remove(file)
 
+def process_data(Station):
+    for file in os.listdir("C:/Users/Evan Hardt/Desktop/Picomation Data/AB4EJ"):
+        date= str(file).split("-")[1]
+        year= date[0:4]
+        month= date[4:6]
+        day= date[6:8]
+
+        os.chdir("C:/Users/Evan Hardt/Desktop/Picomation Data/AB4EJ")
+        filename= "OBS"+year+"-"+month+"-"+day+"T00:00"
+        os.mkdir(filename)
+        #shutil.move(file, filename)
+        #shutil.make_archive(filename, "zip")
+
+        #os.remove(file)
     
+
+def clean_Local_Storage(Station):
+    os.remove(Station['Local_Path'])
 
 def main():
     SERVER_ADDRESS= "10.116.35.24"
@@ -27,7 +51,7 @@ def main():
     
     client= SSHClient()
     client.set_missing_host_key_policy(AutoAddPolicy())
-    PSWS= client.connect(SERVER_ADDRESS, username=USERNAME, password=PASSWD)
+    #PSWS= client.connect(SERVER_ADDRESS, username=USERNAME, password=PASSWD)
 
     stations=[
         {'Name': "AB4EJ",
@@ -38,7 +62,8 @@ def main():
          'Num_Files': 0}]
     
     for station in stations:
-        fetch_magData(station)
+        #fetch_magData(station)
+        process_data(station)
 
 
 if __name__ == "__main__":
